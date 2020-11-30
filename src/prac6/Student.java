312b2a31a7e7fc26a8735236f7902e4ca200997e
Drevomukh <70793125+Drevomukh@users.noreply.github.com>
2020-11-30 21:52:47 +0300

    first commit

---
+++ src/prac6/Student.java
@@ -0,0 +1,22 @@
+package prac6;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String Gruppa;
+
+    public Student(int id, String name, String Gruppa) {
+        this.id = id;
+        this.name = name;
+        this.Gruppa = Gruppa;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
